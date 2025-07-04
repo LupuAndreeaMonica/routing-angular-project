268b71f65b397524141b3fd45c77794a34ab3ff8
Andreea Lupu <andreea.lupu@scriptics.ro>
2025-07-04 10:43:55 +0300

    resolveing route-related dynamic data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,42 @@
-import { Routes } from "@angular/router";
-import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
-import { userRoutes } from './users/users.routes'
+import {Routes} from "@angular/router";
+import {NoTaskComponent} from "./tasks/no-task/no-task.component";
+import {resolveUserName, UserTasksComponent} from "./users/user-tasks/user-tasks.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {userRoutes} from './users/users.routes'
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: NoTaskComponent
+  {
+    path: '',
+    component: NoTaskComponent
+  },
+  {
+    path: 'users/:userId',
+    component: UserTasksComponent,
+    children: userRoutes,
+    data: {
+      message: 'Hello:)'
     },
-    {
-        path: 'users/:userId',
-        component: UserTasksComponent,
-        children: userRoutes
-        // children: [
-        //     {
-        //         path: '',
-        //         redirectTo: 'tasks',
-        //         pathMatch: 'full'
-        //     },
-        //     {
-        //         path: 'tasks',
-        //         component: TasksComponent
-        //     },
-        //     {
-        //         path: 'tasks/new',
-        //         component: NewTaskComponent
-        //     }
-        // ]
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
+    resolve: {
+      userName: resolveUserName
     }
+    // children: [
+    //     {
+    //         path: '',
+    //         redirectTo: 'tasks',
+    //         pathMatch: 'full'
+    //     },
+    //     {
+    //         path: 'tasks',
+    //         component: TasksComponent
+    //     },
+    //     {
+    //         path: 'tasks/new',
+    //         component: NewTaskComponent
+    //     }
+    // ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ]
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,13 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
-import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import {Component, computed, inject, input, OnInit} from '@angular/core';
+import {UsersService} from '../users.service';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -11,21 +18,29 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
     RouterOutlet, RouterLink
   ]
 })
-export class UserTasksComponent implements OnInit {
-private activatedRoute = inject(ActivatedRoute);
-private usersService = inject(UsersService);
-userName = ''
+export class UserTasksComponent {
+  // private activatedRoute = inject(ActivatedRoute);
+  // private usersService = inject(UsersService);
+  userName = input.required<string>();
+  message = input.required<string>();
 
-ngOnInit(): void {
-  console.log(this.activatedRoute)
-  this.activatedRoute.paramMap.subscribe({
-    next: paramMap=> this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || ''
-  })
-}
+  // ngOnInit(): void {
+  //   console.log('input data', this.message())
+  //   // console.log(this.activatedRoute.snapshot)
+  //   // console.log(this.activatedRoute.snapshot.paramMap.get('userId'))
+  //   this.activatedRoute.paramMap.subscribe({
+  //     next: paramMap => this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || ''
+  //   })
+  // }
+
   // userId = input.required<string>();
   // private usersService = inject(UsersService);
-  // userName = computed(() => 
+  // userName = computed(() =>
   //   this.usersService.users.find(u => u.id === this.userId())?.name
   // );
-  
+}
+export const resolveUserName: ResolveFn<string> = (activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot) => {
+  const userService = inject(UsersService);
+  const userName = this.usersService.users.find(u => u.id === activatedRoute.paramMap.get('userId'))?.name || ''
+  return
 }
