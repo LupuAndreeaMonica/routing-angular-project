00d4ff118a11db46bcf7e8ad41449bcf8715e167
Andreea Lupu <andreea.lupu@scriptics.ro>
2025-07-09 11:34:54 +0300

    add service as a provider

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -2,24 +2,31 @@ import { Routes } from '@angular/router';
 
 import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
+import {TasksService} from "../tasks/tasks.service";
 
 export const userRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      }
+    ]
   },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
-  }
 ];
